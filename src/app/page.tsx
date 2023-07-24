b2a6bddc05be4f752bf63fe27fef1b3be9f27cc8
Bart Nagel <bart@tremby.net>
2023-07-24 15:45:05 -0700

    Add imgix image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import styles from './page.module.css'
+import Imgix from "react-imgix";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Imgix src="https://assets.imgix.net/examples/pione.jpg" />
     </main>
   )
 }
